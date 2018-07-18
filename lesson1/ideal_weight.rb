eb8d209ae6af5d5c2309d30e16c2ae32efad516a
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-07-18 20:44:16 +0300

    make separate variable for ideal weight

--- lesson1/ideal_weight.rb
+++ lesson1/ideal_weight.rb
@@ -3,12 +3,14 @@ name = gets.chomp
 puts "Введите Ваш рост в сантиметрах"
 height = gets.chomp.to_f
 
+ideal_weight = height - 110
+
 if height <= 0
   puts "Некорректный рост. Пожалуйста, перезапустите программу и введите
   рост в виде положительного целого или десятичного дробного числа в
   сантиметрах"
-elsif height - 110 >= 0
-  puts "#{name}, Ваш идеальный вес: #{height-110} кг"
+elsif ideal_weight >= 0
+  puts "#{name}, Ваш идеальный вес: #{ideal_weight} кг"
 else
   puts "Ваш вес уже оптимальный"
 end
