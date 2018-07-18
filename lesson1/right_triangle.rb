eded8d30733318af38ef8098e569c981d320c9a9
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-07-18 20:59:55 +0300

    Use Array class methods for triangle sides sorting

--- lesson1/right_triangle.rb
+++ lesson1/right_triangle.rb
@@ -5,31 +5,21 @@ side2 = gets.chomp.to_f
 puts "Введите третью сторону треугольника в любых единицах"
 side3 = gets.chomp.to_f
 
-if (side1 <= 0) || (side2 <=0) || (side3 <= 0)
+if (side1 <= 0) || (side2 <= 0) || (side3 <= 0)
   puts "Стороны треугольника должны быть положительными числами, перезапустите
   программу с корректными значениями"
 else
-  potential_hep = side1
-  potential_cat1 = side2
-  potential_cat2 = side3
+  sorted_sides = [side1, side2, side3].sort
+  hyp = sorted_sides[2]
+  cat1 = sorted_sides[1]
+  cat2 = sorted_sides[0]
 
-  if side2 > side1 && side2 > side3
-    potential_hep = side2
-    potential_cat1 = side1
-    potential_cat2 = side3
-  elsif side3 > side1 && side3 > side2
-    potential_hep = side3
-    potential_cat1 = side1
-    potential_cat2 = side2
-  end
-
-  if ((potential_cat1**2 + potential_cat2**2) - potential_hep**2).abs < 0.000001
+  if ((cat1**2 + cat2**2) - hyp**2).abs < 0.000001
     print "Треугольник прямоугольный "
-    print "и равнобедренный" if potential_cat1 == potential_cat2
+    print "и равнобедренный" if cat1 == cat2
     puts
   else
     puts "Введенные значения не являются длинами сторон прямоугольного
     треугольника"
   end
-
 end
