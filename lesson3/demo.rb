88f4dc11ac0bba55855960899dfdd440213f75d4
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2018-07-25 16:14:23 +0300

    Add some methods RailwaySystem class

--- lesson3/demo.rb
+++ lesson3/demo.rb
@@ -6,37 +6,44 @@ require_relative 'passenger_train.rb'
 require_relative 'coach.rb'
 require_relative 'passenger_coach.rb'
 require_relative 'cargo_coach.rb'
+require_relative 'railway_system.rb'
 
-def seed
+
+def seed(system)
+  system = RailwaySystem.new
   # test create stations
-  petergof1 = Station.new('Новый Петергоф')
-  petergof2 = Station.new('Старый Петергоф')
-  uni = Station.new('Университет')
-  mar = Station.new('Мартышкино')
-  lomonosov = Station.new('Ломоносов')
-  sosnovka = Station.new('Сосновый Бор')
+  system.create_station(Station.new('Новый Петергоф'))
+  system.create_station(Station.new('Старый Петергоф'))
+  system.create_station(Station.new('Университет'))
+  system.create_station(Station.new('Мартышкино'))
+  system.create_station(Station.new('Ломоносов'))
+  system.create_station(Station.new('Сосновый Бор'))
 
   # test create route
-  route1 = Route.new(petergof1, lomonosov)
-  # test adding stations to the route
-  route1.add_station(petergof2)
-  route1.add_station(uni)
-  route1.add_station(mar)
-  # test returning the list of all stations
-  route2 = Route.new(mar, petergof2)
-  route2.add_station(uni)
-  route2.add_station(lomonosov)
-  route2.add_station(mar)
-  route2.add_station(sosnovka)
-  lastochka = PassengerTrain.new('777')
-  10.times { lastochka.add_coach(PassengerCoach.new) }
-  lastochka.route = route1
-
-  old_fashioned_train = CargoTrain.new('001')
-  99.times { old_fashioned_train.add_coach(CargoCoach.new) }
-  old_fashioned_train.route = route2
-
-  lastochka2 = PassengerTrain.new('778')
-  12.times { lastochka2.add_coach(PassengerCoach.new) }
-  lastochka2.route = route1
+  # route1 = Route.new(petergof1, lomonosov)
+  # # test adding stations to the route
+  # route1.add_station(petergof2)
+  # route1.add_station(uni)
+  # route1.add_station(mar)
+  # # test returning the list of all stations
+  # route2 = Route.new(mar, petergof2)
+  # route2.add_station(uni)
+  # route2.add_station(lomonosov)
+  # route2.add_station(mar)
+  # route2.add_station(sosnovka)
+  # lastochka = PassengerTrain.new('777')
+  # 10.times { lastochka.add_coach(PassengerCoach.new) }
+  # lastochka.route = route1
+  #
+  # old_fashioned_train = CargoTrain.new('001')
+  # 99.times { old_fashioned_train.add_coach(CargoCoach.new) }
+  # old_fashioned_train.route = route2
+  #
+  # lastochka2 = PassengerTrain.new('778')
+  # 12.times { lastochka2.add_coach(PassengerCoach.new) }
+  # lastochka2.route = route1
 end
+
+railw = RailwaySystem.new
+seed(railw)
+puts railw.stations_list_str
--- lesson3/railway_system.rb
+++ lesson3/railway_system.rb
@@ -1,11 +1,37 @@
 class RailwaySystem
   def initialize()
-    # создаем массивы для станций, поездов, маршрутов
+    @stations_list = []
+    @train_list = []
+    @route_list = []
   end
 
   # Создать станцию, получить станцию, список станций
+  def create_station(station)
+    stations_list << station
+  end
+
+  def station(index)
+    station.at(index)
+  end
+
+  def stations_list_str
+    res_str = ""
+    stations_list.reduce(res_str) do |res_str, station|
+      res_str + "#{stations_list.find_index(station)}: #{station}"
+    end
+  end
 
   # Создать поезд, получить поезд, список поездов
+  def create_train(train)
+    trains_list << train
+  end
 
   #Создать маршрут, получить маршрут, список маршрутов
+  def create_route(route)
+    route_list << route
+  end
+
+  private
+
+  attr_reader :stations_list, :train_list, :route_list
 end
